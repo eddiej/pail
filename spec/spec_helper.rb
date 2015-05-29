70babdc4a1146628f3f7527e7d6dc48b1496524b
Eddie Johnston <eddie@beanstalk.ie>
2015-05-29 23:49:51 +0100

    change spec_helper to work with engine.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'bundler/setup'
+Bundler.setup
+
+#$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pail'
 
 RSpec.configure do |config|
